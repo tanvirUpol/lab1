8c196f66d876f161671c184325d6babdc12c85f9
Upol <Upol@DESKTOP-U0NT4HT>
2022-05-30 22:12:17 +0600

    Add project files.

---
+++ CV/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CV.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cv()
+        {
+            return View();
+        }
+    }
+}
